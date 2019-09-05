b0ed3d4f67d26153aa9aa0fc0f7076e52c2cd537
Andre Luiz Pastore Dambros <andreluizpd@gmail.com>
2019-09-05 09:34:27 -0300

    Teste para adição de campos no banco

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -2,8 +2,11 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\Photo;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class PhotoController extends AbstractController
 {
@@ -18,5 +21,33 @@ class PhotoController extends AbstractController
     return $this->render('photo/index.html.twig', [
       'number' => $number,
     ]);
+  }
+
+
+  /**
+   * @Route("/save")
+   */
+  public function save()
+  {
+    $entityManager = $this->getDoctrine()->getManager();
+
+    // Photo
+    //  tamanho
+    //  preco
+    //  copias
+    //  photoFileName
+
+
+    $photo = new Photo();
+    $photo->setTamanho("10X15");
+    $photo->setPreco(15.75);
+    $photo->setCopias(7);
+    $photo->setPhotoFileName("C:\\temp\\teste.txt");
+
+    $entityManager->persist($photo);
+
+    $entityManager->flush();
+
+    return new Response("saved photo with id of " . $photo->getId());
   }
 }
